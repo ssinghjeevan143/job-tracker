74a090efd466a414358461c3a7410fc45b47f60c
Jeevan Rajput <s.singhjeevan1234@gmail.com>
2026-09-12 17:04:44 +0530

    Fixed CORS issue for production

--- backend/src/main/java/com/jeevan/jobtracker/controller/JobApplicationController.java
+++ backend/src/main/java/com/jeevan/jobtracker/controller/JobApplicationController.java
@@ -12,7 +12,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/applications")
-@CrossOrigin(origins = "http://localhost:5173")
+@CrossOrigin(origins = "*", allowedHeaders = "*")
 public class JobApplicationController {
 
     private final JobApplicationService service;
